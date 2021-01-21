61d3755bed54950115514ca5c431f416840c914e
张俊超 <zhangjc11@yusys.com.cn>
2021-01-21 18:10:04 +0800

    20210121

--- src/main/java/org/springframework/retry/annotation/Retryable.java
+++ src/main/java/org/springframework/retry/annotation/Retryable.java
@@ -38,6 +38,9 @@ import java.lang.annotation.Target;
 public @interface Retryable {
 
 	/**
+	 * 指定当前class中方法名称用于数据恢复，且指定的方法必须带有@Recover注解
+	 * 如果当前class只有一个带有@Recover注解方法，则默认不设置recover值也会使用该方法，recover属性主要用于当存在多个@Recover注解方法场景
+	 *
 	 * Name of method in this class to use for recover. Method had to be marked with
 	 * {@link Recover} annotation.
 	 * @return the name of recover method
@@ -47,6 +50,16 @@ public @interface Retryable {
 	/**
 	 * Retry interceptor bean name to be applied for retryable method. Is mutually
 	 * exclusive with other attributes.
+	 *
+	 * interceptor指定bean名称，该属性和其它属性相互排斥
+	 * @Bean("myInterceptor")
+	 * public RetryOperationsInterceptor myInterceptor() {
+	 * 		return RetryInterceptorBuilder.stateless()
+	 *                 .backOffOptions(1000, 2, 5000)
+	 *                 .maxAttempts(5).build();
+	 * }
+	 *
+	 *
 	 * @return the retry interceptor bean name
 	 */
 	String interceptor() default "";
@@ -54,6 +67,11 @@ public @interface Retryable {
 	/**
 	 * Exception types that are retryable. Synonym for includes(). Defaults to empty (and
 	 * if excludes is also empty all exceptions are retried).
+	 *
+	 * 被注解的方法发生异常时会重试，value属性指定发生什么异常才会进行重试，默认空对所有异常进行重试
+	 * value空默认对所有异常都进行重试，exclude可以用来排除什么异常不重试
+	 * include属性等价于value
+	 *
 	 * @return exception types to retry
 	 */
 	Class<? extends Throwable>[] value() default {};
@@ -84,16 +102,24 @@ public @interface Retryable {
 	 * Flag to say that the retry is stateful: i.e. exceptions are re-thrown, but the
 	 * retry policy is applied with the same policy to subsequent invocations with the
 	 * same arguments. If false then retryable exceptions are not re-thrown.
+	 *
+	 * 是否有状态重试
+	 *
 	 * @return true if retry is stateful, default false
 	 */
 	boolean stateful() default false;
 
 	/**
+	 * 最大重试次数(包括第一次调用)，默认3
 	 * @return the maximum number of attempts (including the first failure), defaults to 3
 	 */
 	int maxAttempts() default 3;
 
 	/**
+	 * 最大重试次数，支持el表达式
+	 * 如从环境变量中取：maxAttemptsExpression = "${retry.max}"
+	 * 或者调用bean方法计算：maxAttemptsExpression = "#{@testService01.getMax()}"
+	 *
 	 * @return an expression evaluated to the maximum number of attempts (including the
 	 * first failure), defaults to 3 Overrides {@link #maxAttempts()}.
 	 * @since 1.2
@@ -104,6 +130,16 @@ public @interface Retryable {
 	 * Specify the backoff properties for retrying this operation. The default is a simple
 	 * {@link Backoff} specification with no properties - see it's documentation for
 	 * defaults.
+	 *
+	 * 设置回避策略
+	 *
+	 * delay(等同于value)-指定延迟后重试，默认为1000L，
+	 * 当未设置multiplier时，表示每隔delay的时间重试，直到重试次数到达maxAttempts设置的最大允许重试次数，
+	 * 当设置了multiplier参数时，该值作为幂运算的初始值（delay = delay * multiplier）；
+	 *
+	 * multiplier-指定延迟倍数，默认为0，表示固定暂停1秒后进行重试。比如delay=5000L,multiplier=2时，第一次重试为5秒后，第二次为10秒，第三次为20秒
+	 *
+	 *
 	 * @return a backoff specification
 	 */
 	Backoff backoff() default @Backoff();
@@ -118,6 +154,11 @@ public @interface Retryable {
 	 * </pre> and <pre class=code>
 	 *  {@code "@someBean.shouldRetry(#root)"}.
 	 * </pre>
+	 *
+	 *
+	 * 抛出异常中包含特定字符串才会进行重试：exceptionExpression = "message.contains('this can be retried')"
+	 * 调用bean方法判断是否支持该异常重试：exceptionExpression = "@exceptionChecker.shouldRetry(#root)"   #root代表抛出异常对象
+	 *
 	 * @return the expression.
 	 * @since 1.2
 	 */
@@ -126,6 +167,9 @@ public @interface Retryable {
 	/**
 	 * Bean names of retry listeners to use instead of default ones defined in Spring
 	 * context
+	 *
+	 * 设置RetryListener的bean名称
+	 *
 	 * @return retry listeners bean names
 	 */
 	String[] listeners() default {};
